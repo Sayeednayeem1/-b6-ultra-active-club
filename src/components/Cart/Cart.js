a0dde2fd0b211095105d68440c688f44d47bf50b
Sayeednayeem1 <sayeedn850@gmail.com>
2022-09-29 00:01:24 +0600

    added sticky details card

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = () => {
     return (
-        <div>
-            <h1>test test test</h1>
+        <div className='cart'>
+            <div className='about-self'>
+                <h4>Name: Sayeed Nayeem</h4>
+                <p>Sylhet, Bangladesh</p>
+            </div>
+            <div  className='body-details'>
+                <div>
+                    <h4>75<span>kg</span></h4>
+                    <p>Weight</p> 
+                </div>
+                <div>
+                    <h4>6<span>Feet</span></h4>
+                    <p>Height</p> 
+                </div>
+                <div>
+                    <h4>25<span>yrs</span></h4>
+                    <p>Age</p> 
+                </div>
+            </div>
+            <div>
+                <h4 className='give-me-margin'>Add A Break</h4>
+                <div className='add-a-break'>
+                    <h5>10s</h5>
+                    <h5>20s</h5>
+                    <h5>30s</h5>
+                    <h5>40s</h5>
+                    <h5>50s</h5>
+                </div>
+            </div>
         </div>
     );
 };
