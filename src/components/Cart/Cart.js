2b1c2f571b1f26fd01fe346b10ae19f39d3981e4
Sayeednayeem1 <sayeedn850@gmail.com>
2022-09-29 15:26:39 +0600

    added alert and remodified some functions

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,6 +2,33 @@ import React, { useState } from 'react';
 import './Cart.css';
 
 const Cart = ({cart}) => {
+    let [test, setTest] = useState(0);
+
+    const thisIsTest = ()=>{
+        test = 10;
+        setTest(test);
+    }
+    const thisIsTest2 = ()=>{
+        test = 20;
+        setTest(test);
+    }
+    const thisIsTest3 = ()=>{
+        test = 30
+        setTest(test);
+    }
+    const thisIsTest4 = ()=>{
+        test = 40;
+        setTest(test);
+    }
+    const thisIsTest5 = ()=>{
+        test = 50;
+        setTest(test);
+    }
+    // toast me
+    const toast = ()=>{
+        return alert('This is a simple alert!');
+    }
+
     let time = 0;
     for(const product of cart){
         time = time + product.time;
@@ -14,7 +41,7 @@ const Cart = ({cart}) => {
             </div>
             <div  className='body-details'>
                 <div>
-                    <h4>75<span>kg</span></h4>
+                    <h4>67<span>kg</span></h4>
                     <p>Weight</p> 
                 </div>
                 <div>
@@ -29,11 +56,11 @@ const Cart = ({cart}) => {
             <div>
                 <h4 className='give-me-margin'>Add A Break</h4>
                 <div className='add-a-break'>
-                    <button>10s</button>
-                    <button>20s</button>
-                    <button>30s</button>
-                    <button>40s</button>
-                    <button>50s</button>
+                    <button onClick={thisIsTest}>10s</button>
+                    <button onClick={thisIsTest2}>20s</button>
+                    <button onClick={thisIsTest3}>30s</button>
+                    <button onClick={thisIsTest4}>40s</button>
+                    <button onClick={thisIsTest5}>50s</button>
                 </div>
             </div>
             <div>
@@ -44,11 +71,11 @@ const Cart = ({cart}) => {
                 </div>
                 <div className='time-maintenance'>
                     <h4>Break Time</h4>
-                    <p>200s</p>
+                    <p>{test}s</p>
                 </div>
             </div>
             <div className='activity-details-btn'>
-                <button className='btn'>
+                <button onClick={toast} className='btn'>
                     <p>Activity Computed</p>
                 </button>
             </div>
