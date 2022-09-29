3e889519f7254179381145ccaa6d02a2b9d9ee0e
Sayeednayeem1 <sayeedn850@gmail.com>
2022-09-29 11:13:40 +0600

    added footer section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Products from './components/Products/Products';
 import Question from './components/Question/Question';
@@ -10,6 +11,7 @@ function App() {
       <Header></Header>
       <Products></Products>
       <Question></Question>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p><small>All Right Reserved @2022</small></p>
+        </div>
+    );
+};
+
+export default Footer;
